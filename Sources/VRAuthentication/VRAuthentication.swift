e7fbe7fae0de24c18c56750b774c80e140563f19
Vinicius Rossado <vinirossado@gmail.com>
2025-12-17 09:29:55 +0100

    Add placeholder files for VRCaching and VRAuthentication modules

---
+++ Sources/VRAuthentication/VRAuthentication.swift
@@ -0,0 +1,17 @@
+//
+//  VRAuthentication.swift
+//  VRFoundation
+//
+//  Placeholder for VRAuthentication module
+//  This module will contain authentication utilities
+//
+
+import Foundation
+
+/// Placeholder for VRAuthentication module
+/// TODO: Implement authentication functionality
+public struct VRAuthentication {
+    public static let version = "1.0.0"
+    
+    private init() {}
+}
---
+++ Sources/VRCaching/VRCaching.swift
@@ -0,0 +1,17 @@
+//
+//  VRCaching.swift
+//  VRFoundation
+//
+//  Placeholder for VRCaching module
+//  This module will contain caching utilities and ETag management
+//
+
+import Foundation
+
+/// Placeholder for VRCaching module
+/// TODO: Implement caching functionality
+public struct VRCaching {
+    public static let version = "1.0.0"
+    
+    private init() {}
+}
